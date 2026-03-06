9c8e9df818f7cdda58609abb600618f97067574d
maira.bolanos <mairacordoba1123@gmail.com>
2026-03-06 09:04:20 -0500

    rafactor: unify all in one project

---
+++ base-de-datos/asistencia.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 06, 2026 at 04:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `asistencia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asistencias`
+--
+
+CREATE TABLE `asistencias` (
+  `id_asistencia` bigint(20) NOT NULL,
+  `estado` enum('AUSENTE','EXCUSADO','PRESENTE') DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `id_clase` bigint(20) DEFAULT NULL,
+  `id_estudiante` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clases`
+--
+
+CREATE TABLE `clases` (
+  `id_clase` bigint(20) NOT NULL,
+  `nombre_clase` varchar(255) DEFAULT NULL,
+  `id_profesor` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clase_estudiante`
+--
+
+CREATE TABLE `clase_estudiante` (
+  `id_clase` bigint(20) NOT NULL,
+  `id_estudiante` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `estudiantes`
+--
+
+CREATE TABLE `estudiantes` (
+  `id_estudiante` bigint(20) NOT NULL,
+  `codigo` varchar(255) DEFAULT NULL,
+  `nombre_estudiante` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profesores`
+--
+
+CREATE TABLE `profesores` (
+  `id_profesor` bigint(20) NOT NULL,
+  `cedula` varchar(255) DEFAULT NULL,
+  `contrasena` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `nombre_usuario` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `asistencias`
+--
+ALTER TABLE `asistencias`
+  ADD PRIMARY KEY (`id_asistencia`),
+  ADD KEY `FKfttra7nxmltc1ggtkyrk89gt3` (`id_clase`),
+  ADD KEY `FKi9ni3pi9ev151cy0qq6s0taw0` (`id_estudiante`);
+
+--
+-- Indexes for table `clases`
+--
+ALTER TABLE `clases`
+  ADD PRIMARY KEY (`id_clase`),
+  ADD KEY `FKjuih704v7agr6ummyum4sfg6h` (`id_profesor`);
+
+--
+-- Indexes for table `clase_estudiante`
+--
+ALTER TABLE `clase_estudiante`
+  ADD KEY `FKi3ql2y00hxyslf3vjf7rxslol` (`id_estudiante`),
+  ADD KEY `FKo29xuae8opp55756we9vkrrkh` (`id_clase`);
+
+--
+-- Indexes for table `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  ADD PRIMARY KEY (`id_estudiante`);
+
+--
+-- Indexes for table `profesores`
+--
+ALTER TABLE `profesores`
+  ADD PRIMARY KEY (`id_profesor`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `asistencias`
+--
+ALTER TABLE `asistencias`
+  MODIFY `id_asistencia` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clases`
+--
+ALTER TABLE `clases`
+  MODIFY `id_clase` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  MODIFY `id_estudiante` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profesores`
+--
+ALTER TABLE `profesores`
+  MODIFY `id_profesor` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `asistencias`
+--
+ALTER TABLE `asistencias`
+  ADD CONSTRAINT `FKfttra7nxmltc1ggtkyrk89gt3` FOREIGN KEY (`id_clase`) REFERENCES `clases` (`id_clase`),
+  ADD CONSTRAINT `FKi9ni3pi9ev151cy0qq6s0taw0` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiantes` (`id_estudiante`);
+
+--
+-- Constraints for table `clases`
+--
+ALTER TABLE `clases`
+  ADD CONSTRAINT `FKjuih704v7agr6ummyum4sfg6h` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`);
+
+--
+-- Constraints for table `clase_estudiante`
+--
+ALTER TABLE `clase_estudiante`
+  ADD CONSTRAINT `FKi3ql2y00hxyslf3vjf7rxslol` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiantes` (`id_estudiante`),
+  ADD CONSTRAINT `FKo29xuae8opp55756we9vkrrkh` FOREIGN KEY (`id_clase`) REFERENCES `clases` (`id_clase`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
